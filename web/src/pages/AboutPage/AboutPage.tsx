c12384324241183d5b38ce9ccfa42e3605d2c249
JamesJ995 <mrjam7@gmail.com>
2025-01-22 23:18:16 -0600

    tutorial commit

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+
+      <p>This is my tutorial redwoodJS Blog, ~LotR quote here~</p>
+    </>
+  )
+}
+
+export default AboutPage
